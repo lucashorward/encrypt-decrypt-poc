1b196f354e722b5d766ee13f3029d8e0ae417382
lucashorward <lucas.horward@gmail.com>
2020-05-21 12:47:52 +0200

    Added decrypt & file function

--- src/main.rs
+++ src/main.rs
@@ -7,41 +7,65 @@ use std::io::Read;
 use std::io::prelude::*;
 
 fn main() {
-    let file_name = std::env::args().nth(1).expect("no file name given");
-    encrypt_file(&file_name);
+    let action = std::env::args().nth(1).expect("no action given. Please enter \"encrypt\" or \"decrypt\" followed by a path");
+    let file_name = std::env::args().nth(2).expect("no file name given");
+    if action == "encrypt" {
+        encrypt_file(&file_name);
+    } else if action == "decrypt" {
+        decrypt_file(&file_name);
+    } else {
+        println!("Unknown action: {}", action);
+    }
 }
 
-fn encrypt_file(path: &String) -> std::io::Result<()> {
-    println!("Enter a key. It has to be EXACTLY 12 characters.");
+fn encrypt_file(path: &String) {
+    println!("Enter a key. It has to be EXACTLY 32 characters.");
     let stdin = io::stdin();
     let user_filled_key = stdin.lock().lines().next().unwrap().unwrap(); 
-    if user_filled_key.len() != 12 {
-        panic!("Key was not 12 characters: {:?}", user_filled_key);
+    if user_filled_key.len() != 32 {
+        panic!("Key was not 32 characters: {:?}", user_filled_key);
     }
-    let gen = generate_string();
-    let key = GenericArray::clone_from_slice(gen.as_bytes());
+    let key = GenericArray::clone_from_slice(user_filled_key.as_bytes());
     let aead = Aes256GcmSiv::new(key);
-    let nonce = GenericArray::from_slice(user_filled_key.as_bytes());
+    let gen = generate_string();
+    println!("Save this nonce, without it you cannot decrypt!");
+    println!("{:?}", gen);
+    let nonce = GenericArray::from_slice(gen.as_bytes());
     let file = get_file_as_byte_vec(path);
     let ciphertext = aead
     .encrypt(nonce, file.as_ref())
     .expect("encryption failure!");
-    println!("{:?}", ciphertext);
-    let new_path = format!("{}.encrypted", path);
-    let mut file = File::create(new_path)?;
-    file.write_all(ciphertext.as_ref())?;
-    decrypt_value(gen, ciphertext, user_filled_key);
-    Ok(())
-    // TODO read from file
+    write_to_file(path, ciphertext);
 }
 
-fn decrypt_value(key: String, value: Vec<u8>, nonce_string: String) {
+fn decrypt_file(path: &String) {
+    println!("Enter your 32-character key");
+    let stdin = io::stdin();
+    let user_filled_key = stdin.lock().lines().next().unwrap().unwrap(); 
+    if user_filled_key.len() != 32 {
+        panic!("Key was not 32 characters: {:?}", user_filled_key);
+    }
+    println!("Enter your 12-character random nonce");
+    let nonce = stdin.lock().lines().next().unwrap().unwrap(); 
+    if nonce.len() != 12 {
+        panic!("Key was not 12 characters: {:?}", user_filled_key);
+    }
+    let file = get_file_as_byte_vec(path);
+    let val = decrypt_value(user_filled_key, file, nonce);
+    write_to_file(path, val);
+}
+
+fn write_to_file(path: &String, data: Vec<u8>) -> std::io::Result<()> {
+    let mut file = File::create(path)?;
+    file.write_all(data.as_ref())?;
+    Ok(())
+}
+
+fn decrypt_value(key: String, value: Vec<u8>, nonce_string: String) -> Vec<u8> {
     let generated_key = GenericArray::clone_from_slice(key.as_bytes());
     let aead = Aes256GcmSiv::new(generated_key);
     let nonce = GenericArray::from_slice(nonce_string.as_bytes()); // 96-bits; unique per message
-    let plaintext = aead.decrypt(nonce, value.as_ref()).expect("decryption failure!");
-    let txt = std::str::from_utf8(&plaintext).unwrap();
-    println!("{:?}", txt);
+    aead.decrypt(nonce, value.as_ref()).expect("decryption failure!")
 }
 
 fn generate_string() -> String {
@@ -49,7 +73,7 @@ fn generate_string() -> String {
     const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                             abcdefghijklmnopqrstuvwxyz\
                             0123456789)(*&^%$#@!~";
-    const PASSWORD_LEN: usize = 32;
+    const PASSWORD_LEN: usize = 12;
     let mut rng = rand::thread_rng();
 
     let password: String = (0..PASSWORD_LEN)
